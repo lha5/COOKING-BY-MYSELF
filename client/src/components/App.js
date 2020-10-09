eb0faa4d96cb3ecd39f23e0d73171922944d695b
lha5 <hyeahnlee5@gmail.com>
2020-10-09 15:42:39 +0900

    commit test...

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch
-} from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css'
 
+import Auth from '../hoc/auth';
+import NavBar from './view/NavBar/NavBar';
 import LandingPage from './view/LandingPage/LandingPage';
+import UploadProductPage from './view/UploadProductPage/UploadProductPage';
+import Footer from './view/Footer/Footer';
 
 function App() {
   return (
-    <Router>
-        <div>
-            <Switch>
-                <Route exact path="/" component={LandingPage} />
-            </Switch>
-        </div>
-    </Router>
+    <Suspense fallback={(<div>LOADING...</div>)}>
+        <NavBar />
+        <Switch>
+            <Route exact path="/" component={Auth(LandingPage, null)} />
+            {/*<Route exact path="/user/signin" component={Auth(SigninPage, false)} />*/}
+            <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
+        </Switch>
+        <Footer />
+    </Suspense>
   );
 }
 
---
+++ client/src/components/view/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UploadProductPage(props) {
+    return (
+        <div>
+            상품 업로드 페이지
+        </div>
+    );
+}
+
+export default UploadProductPage;
