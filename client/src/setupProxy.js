01f69eb02c6ad51de597fa80fe044618b8ec5960
lha5 <hyeahnlee5@gmail.com>
2020-10-18 22:38:25 +0900

    setting for client - Netlify

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'https://cooking-by-myself.herokuapp.com',
             changeOrigin: true
         })
     );
